d709514283945b33d9e56af28c05321fc68f04e4
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-01-17 13:20:05 +0100

    Wrapped my array into an IIFE and changed the for loop into an foreach loop

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
-let pokemonList = [
-    { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying'] },
-    { name: 'Typhlosion', height: 1.7, type: ['Fire'] },
-    { name: 'Tyranitar', height: 2, type: ['Dark', 'Rock'] }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying'] },
+        { name: 'Typhlosion', height: 1.7, type: ['Fire'] },
+        { name: 'Tyranitar', height: 2, type: ['Dark', 'Rock'] }
+    ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ',);
-    if (pokemonList[i].height <= 1.0) {
-        document.write('<br>');
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-    else {
-        document.write('- Wow, that\'s big!');
-        document.write('<br>');
+
+    function getAll() {
+        return pokemonList;
     }
-}
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write("<p>Name: " + pokemon.name + ", Height: " + pokemon.height + "m, Type: " + pokemon.type.join(', '));
+
+    if (pokemon.height >= 2) {
+        document.write(" - Wow, that's big!");
+    }
+
+    document.write("</p>");
+});
+
+
+
+
+
 
 
 
