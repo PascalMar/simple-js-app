c23b5a68ac7036d7d3bc168993c4175b4b956792
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-01-29 13:15:44 +0100

    API added and functions updated, loading message added, and also added polyfill for older browsers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying'] },
-        { name: 'Typhlosion', height: 1.7, type: ['Fire'] },
-        { name: 'Tyranitar', height: 2, type: ['Dark', 'Rock'] }
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -25,26 +22,70 @@ let pokemonRepository = (function () {
         pokemonList.appendChild(listpokemon);
     }
 
+    function loadList() {
+        showLoadingMessage();
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            hideLoadingMessage();
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        showLoadingMessage();
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            hideLoadingMessage();
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
     function showDetails(pokemon) {
-        console.log("Details for:", pokemon);
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
     }
 
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
         showDetails: showDetails
     };
 })();
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
+pokemonRepository.loadList().then(function () {
     pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
 });
 
+
+function showLoadingMessage() {
+    document.getElementById('loadingMessage').style.display = 'block';
+}
+
+function hideLoadingMessage() {
+    document.getElementById('loadingMessage').style.display = 'none';
+}
 
 
 
